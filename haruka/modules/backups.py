b30d9d7ed20c941689fb8902693f7bcfbe9d3cf9
SUSHANT GIRDHAR <52759137+sushantgirdhar@users.noreply.github.com>
2020-04-28 13:30:41 +0530

    Update backups.py

--- haruka/modules/backups.py
+++ haruka/modules/backups.py
@@ -272,7 +272,7 @@ def export_data(bot: Bot, update: Update, chat_data):
 	# Backing up
 	backup[chat_id] = {'bot': bot.id, 'hashes': {'info': {'rules': rules}, 'extra': notes, 'blacklist': bl, 'disabled': disabledcmd, 'locks': locked}}
 	baccinfo = json.dumps(backup, indent=4)
-	f=open("harukab{}.backup".format(chat_id), "w")
+	f=open("ebruiser{}.backup".format(chat_id), "w")
 	f.write(str(baccinfo))
 	f.close()
 	bot.sendChatAction(current_chat_id, "upload_document")
@@ -281,8 +281,8 @@ def export_data(bot: Bot, update: Update, chat_data):
 		bot.sendMessage(MESSAGE_DUMP, "*Successfully imported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`".format(chat.title, chat_id, tgl), parse_mode=ParseMode.MARKDOWN)
 	except BadRequest:
 		pass
-	bot.sendDocument(current_chat_id, document=open('harukab{}.backup'.format(chat_id), 'rb'), caption="*Successfully imported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `harukab` is specially made for notes.".format(chat.title, chat_id, tgl), timeout=360, reply_to_message_id=msg.message_id, parse_mode=ParseMode.MARKDOWN)
-	os.remove("harukab{}.backup".format(chat_id)) # Cleaning file
+	bot.sendDocument(current_chat_id, document=open('ebruiser{}.backup'.format(chat_id), 'rb'), caption="*Successfully imported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `ebruiser` is specially made for notes.".format(chat.title, chat_id, tgl), timeout=360, reply_to_message_id=msg.message_id, parse_mode=ParseMode.MARKDOWN)
+	os.remove("ebruiser{}.backup".format(chat_id)) # Cleaning file
 
 
 # Temporary data
