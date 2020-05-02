35f49a230e1cc0f1c31375df7a86a4dc738e9fb3
sushantgirdhar1 <53021053+sushantgirdhar1@users.noreply.github.com>
2020-05-02 13:10:59 +0530

    Update chat_status.py

--- haruka/modules/helper_funcs/chat_status.py
+++ haruka/modules/helper_funcs/chat_status.py
@@ -165,3 +165,21 @@ def user_is_gbanned(func):
             pass
     return is_user_gbanned
 
+def dev_plus(func):
+    @wraps(func)
+    def is_dev_plus_func(bot: Bot, update: Update, *args, **kwargs):
+
+        user = update.effective_user
+
+        if user.id in DEV_USERS:
+            return func(bot, update, *args, **kwargs)
+        elif not user:
+            pass
+        elif DEL_CMDS and " " not in update.effective_message.text:
+            update.effective_message.delete()
+        else:
+            update.effective_message.reply_text("This is a developer restricted command."
+                                                " You do not have permissions to run this.")
+
+    return is_dev_plus_fun
+
