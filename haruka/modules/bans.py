0a20c63ae10107b39a92f6b685155972bd261aaf
sushantgirdhar1 <53021053+sushantgirdhar1@users.noreply.github.com>
2020-05-06 17:06:56 +0530

    Update bans.py

--- haruka/modules/bans.py
+++ haruka/modules/bans.py
@@ -6,7 +6,7 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from haruka import dispatcher, LOGGER, DEV_USERS, SUDO_USERS, TIGER_USERS
+from haruka import dispatcher, LOGGER, SUDO_USERS,
 from haruka.modules.disable import DisableAbleCommandHandler
 from haruka.modules.helper_funcs.chat_status import (bot_admin, user_admin, is_user_ban_protected, can_restrict,
                                                      is_user_admin, is_user_in_chat, connection_status)
